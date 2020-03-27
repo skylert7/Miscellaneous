3846504940201b7a71f1379b0bbfae1b1956623e
skylert7 <skyler.linhtran@gmail.com>
2020-03-26 23:51:15 -0500

    updated phase1

--- Information_Retrieval/Project1/project1.py
+++ Information_Retrieval/Project1/project1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import sys
+import operator
 
 tags = ["a", "abbr", "acronym", "address", "area", "b", "base", "bdo", "big", "blockquote", "body", "br", "button",
         "caption", "cite", "code", "col", "colgroup", "dd", "del", "dfn", "div", "dl", "DOCTYPE", "dt", "em",
@@ -16,16 +17,17 @@ class WebCrawl():
     def __init__(self, domain, N):
         self.N = N # limit on the number of pages to retrieve
         self.domain = domain
+        self.url = ""
         self.raw_content = ""
         self.content_dict = dict()
-        self.word_vector = list()
         self.disallow = list()
         self.allow = list()
         self.broken_links = list()
         self.non_text_urls = list()
         self.titles = dict()
         self.num_of_documents_indexed = 0
-        self.words_indexed = 0
+        self.document_matrix = dict()
+        self.totalWordsInDocument = dict()
 
     def isNoIndexPage(self):
         soup = BeautifulSoup(self.raw_content, 'html.parser')
@@ -60,9 +62,24 @@ class WebCrawl():
             if i.split(": ")[0] == "Disallow":
                 url = prefix + self.domain + i.split(": ")[1]
                 self.disallow.append(url)
+
+                #Check for non-text url
+                if any(ext in url for ext in text_file_extension): # if any text ext found in the url
+                    print('', end='')
+                else: # if not found, append the url to non_text_urls
+                    if url not in self.non_text_urls:
+                        self.non_text_urls.append(url)
+
             if i.split(": ")[0] == "Allow":
                 url = prefix + self.domain + i.split(": ")[1]
                 self.allow.append(url)
+
+                #Check for non-text url
+                if any(ext in url for ext in text_file_extension): # if any text ext found in the url
+                    print('', end='')
+                else: # if not found, append the url to non_text_urls
+                    if url not in self.non_text_urls:
+                        self.non_text_urls.append(url)
 
     def parse_html_and_make_word_vector(self):
         soup = BeautifulSoup(self.raw_content, 'html.parser')
@@ -75,13 +92,23 @@ class WebCrawl():
                 if content.string != None:
                     content_list_of_each_tag.append(content.string)
 
-        # making word vector
+
+        # making document_matrix
+        # {word: [(word count, url1), (word count, url2)]
         for i in range(len(content_list_of_each_tag)):
             for word in content_list_of_each_tag[i].split():
-                self.word_vector.append(word)
+                if word not in self.document_matrix.keys():
+                    self.document_matrix[word] = [(1, self.url)]
+                else:
+                    for i in range(len(self.document_matrix[word])):
+                        if self.document_matrix[word][i][1] == self.url:
+                            self.document_matrix[word][i] = (self.document_matrix[word][i][0] + 1, self.url)
+                            break
+                        else:
+                            if i == len(self.document_matrix[word]) - 1:
+                                self.document_matrix[word].append((1, self.url))
 
     def getTitle(self):
-
         for url in self.disallow:
             self.getRawContentFromTextFile(url)
             if self.raw_content == "":
@@ -121,23 +148,25 @@ class WebCrawl():
 
     def getRawContentFromTextFile(self, url):
         try:
-            # with open(url, "r") as f:
-            #     page = f.read()
-            # self.raw_content = page
-
             r = requests.get(url)
             status_code = r.status_code
         except:
             self.raw_content = ""
+            self.url = ""
             return
 
         if status_code != 200:
             print("Error encountered in {} with status code {}".format(url, status_code))
             self.broken_links.append(url)
             self.raw_content = ""
+            self.url = ""
             return
 
+        self.url = url #Keep track of what url is being processed
         self.raw_content = r.content.decode('utf-8')
+
+    def getTotalWordsIndexed(self):
+        return
 
     def crawl(self):
         self.create_site_map()
@@ -146,6 +175,9 @@ class WebCrawl():
 
         for i in range(len(self.allow)):
             url = self.allow[i]
+            if url in self.non_text_urls:
+                continue
+
             if i <= self.N:
                 self.getRawContentFromTextFile(url=url)
                 if self.isNoIndexPage(): # has meta tag of noindex, move on
@@ -158,6 +190,7 @@ class WebCrawl():
                     continue
 
                 self.parse_html_and_make_word_vector()
+                self.num_of_documents_indexed = self.num_of_documents_indexed + 1
                 time.sleep(4) # wait 4 second after crawling a single webpage
 
 if __name__ == '__main__':
@@ -174,4 +207,26 @@ if __name__ == '__main__':
 
     print(my_crawler.titles)
 
+    #Number of document indexed
+    print("Number of document indexed")
+    print(my_crawler.num_of_documents_indexed)
 
+    #Term - Document Matrix
+    print("Term - Document Matrix")
+    print(my_crawler.document_matrix)
+
+    #Top 20 common words
+    print("20 common words")
+
+    word_count = dict()
+
+    for word in my_crawler.document_matrix.keys():
+        total_word_count = 0
+        for index in range(len(my_crawler.document_matrix[word])):
+            total_word_count = total_word_count + my_crawler.document_matrix[word][index][0]
+        word_count[word] = total_word_count
+
+    sorted_word_count = dict(sorted(word_count.items(), key=operator.itemgetter(1), reverse=True))
+
+    for i in range(20):
+        print(list(sorted_word_count)[i])
