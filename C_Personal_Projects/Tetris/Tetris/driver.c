bd2761fb9dd5021303625b4a7f174f75bb5a0d57
skylert7 <skyler.linhtran@gmail.com>
2020-07-25 23:53:30 -0500

    define

--- C_Personal_Projects/Tetris/Tetris/driver.c
+++ C_Personal_Projects/Tetris/Tetris/driver.c
@@ -1,5 +1,10 @@
-#include <stdio.h> 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ROWS 20
+#define COLS 11
+#define TRUE 1
+#define FALSE 0
 // main function - 
 // where the execution of program begins 
 int main()
